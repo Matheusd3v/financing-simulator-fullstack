5034eacc8200b9d765cf4a7ba6764099b2bd4853
Matheus <>
2025-05-31 08:42:58 -0300

    feat: add swagger tag in student controller

--- backend/src/application/modules/students/controllers/student.controller.ts
+++ backend/src/application/modules/students/controllers/student.controller.ts
@@ -1,28 +1,25 @@
-import { Transactional } from "@nestjs-cls/transactional";
-import { Body, Controller, HttpStatus, Post } from "@nestjs/common";
-import { CreateStudentUseCase } from "../usecases/create-student.usecase";
-import { CreateStudentDto } from "../dtos/create-student.dto";
-import { ApiResponse } from "@nestjs/swagger";
-import { SuccessMessage } from "@root/src/application/shared/classes/success-message";
+import { Transactional } from '@nestjs-cls/transactional';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { CreateStudentUseCase } from '../usecases/create-student.usecase';
+import { CreateStudentDto } from '../dtos/create-student.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SuccessMessage } from '@root/src/application/shared/classes/success-message';
 
+@ApiTags('Student')
 @Controller()
 export class StudentController {
-    constructor(
-        private readonly createStudentUseCase: CreateStudentUseCase
-    ) {}
+    constructor(private readonly createStudentUseCase: CreateStudentUseCase) {}
 
     @Post('/register')
     @Transactional()
     @ApiResponse({
         type: SuccessMessage,
-        status: HttpStatus.CREATED
+        status: HttpStatus.CREATED,
     })
-    async register(
-        @Body() body: CreateStudentDto
-    ) {
-        await this.createStudentUseCase.execute(body)
+    async register(@Body() body: CreateStudentDto) {
+        await this.createStudentUseCase.execute(body);
         return {
-            message: 'Estudante Criado!'
-        }
+            message: 'Estudante Criado!',
+        };
     }
 }
