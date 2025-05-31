cec54722005fa62f7ef0143b15e4bd5c717898d4
Matheus <>
2025-05-31 09:37:11 -0300

    fix: register student usecase

--- backend/src/application/modules/students/usecases/create-student.usecase.ts
+++ backend/src/application/modules/students/usecases/create-student.usecase.ts
@@ -1,8 +1,8 @@
-import { ConflictException, Inject, Injectable } from "@nestjs/common";
-import { StudentRepository } from "../repositories/student.repository";
-import { CreateStudentDto } from "../dtos/create-student.dto";
-import { CryptoRepository } from "@infrastructure/crypto/crypto.repository";
-import { StudentEntity } from "../entities/student.entity";
+import { ConflictException, Inject, Injectable } from '@nestjs/common';
+import { StudentRepository } from '../repositories/student.repository';
+import { CreateStudentDto } from '../dtos/create-student.dto';
+import { CryptoRepository } from '@infrastructure/crypto/crypto.repository';
+import { StudentEntity } from '../entities/student.entity';
 
 @Injectable()
 export class CreateStudentUseCase {
@@ -10,34 +10,33 @@ export class CreateStudentUseCase {
         @Inject('StudentRepository')
         private readonly studentRepository: StudentRepository,
         @Inject('CryptoRepository')
-        private readonly cryptoRepository: CryptoRepository
+        private readonly cryptoRepository: CryptoRepository,
     ) {}
 
     public async execute(body: CreateStudentDto) {
-        await this.validateEmail(body.email)
-        
-        const passwordHashed = await this.cryptoRepository.hash(body.password)
+        const passwordHashed = await this.cryptoRepository.hash(body.password);
 
-        const student = new StudentEntity()
-        student.setEmail(body.email)
-        student.setPassword(passwordHashed)
-        student.lastName = body.lastName
-        student.name = body.name
+        const student = new StudentEntity();
+        student.setEmail(body.email);
+        student.setPassword(passwordHashed);
+        student.lastName = body.lastName;
+        student.name = body.name;
 
-        await this.studentRepository.save(student)
+        await this.validateEmail(student.getEmail());
+        await this.studentRepository.save(student);
     }
 
     private async validateEmail(email: string) {
         const student = await this.studentRepository.findOne({
             where: {
-                email
-            }
-        })
+                email,
+            },
+        });
 
         if (student) {
             throw new ConflictException({
-                message: 'Estudante ja cadastrado! Tente outro email'
-            })
+                message: 'Estudante ja cadastrado! Tente outro email',
+            });
         }
     }
 }
