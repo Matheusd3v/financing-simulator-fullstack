30c614b9393cbbc0ee855d9d3a9a8450c91165d5
Matheus <>
2025-05-30 21:20:34 -0300

    feat: add main config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './application/app.module';
 import {
-  FastifyAdapter,
-  NestFastifyApplication,
+    FastifyAdapter,
+    NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+    );
+
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+        }),
+    );
+	app.useStaticAssets({ root: join(__dirname, '..', 'public') });
+
+
+    const config = new DocumentBuilder()
+        .setVersion(process.env.npm_package_version ?? '1')
+        .addBearerAuth()
+        .setTitle('Financing Simulator API')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/doc', app, documentFactory);
+
+    await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
