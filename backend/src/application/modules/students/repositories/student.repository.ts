2ae906b09503d6d0741938dd38f28db6cf5f0cb5
Matheus <>
2025-05-30 21:23:13 -0300

    feat: add student repository

---
+++ backend/src/application/modules/students/repositories/student.repository.ts
@@ -0,0 +1,7 @@
+import { IFindOptionsStudentDto } from '../dtos/find-options.dto';
+import { StudentEntity } from '../entities/student.entity';
+
+export interface StudentRepository {
+    save(entity: StudentEntity): Promise<StudentEntity>;
+    findOne(args: IFindOptionsStudentDto): Promise<StudentEntity | null>;
+}
---
+++ backend/src/infrastructure/database/sql/prisma/repositories/student.repository.ts
@@ -0,0 +1,40 @@
+import { TransactionHost } from '@nestjs-cls/transactional';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@root/generated/prisma/client';
+import { IFindOptionsStudentDto } from '@application/modules/students/dtos/find-options.dto';
+import { StudentEntity } from '@application/modules/students/entities/student.entity';
+import { StudentRepository } from '@application/modules/students/repositories/student.repository';
+import { StudentMap } from '../mappers/student.mapper';
+import { StudentBuilder } from '../builders/student.builder';
+
+@Injectable()
+export class StudentPostgresqlRepository implements StudentRepository {
+    constructor(
+        private readonly prismaService: TransactionHost<
+            TransactionalAdapterPrisma<PrismaClient>
+        >,
+    ) {}
+
+    public async save(entity: StudentEntity): Promise<StudentEntity> {
+        const saved = await this.prismaService.tx.student.create({
+            data: {
+                password: entity.getPassword(),
+                email: entity.getEmail(),
+                lastName: entity.lastName,
+                name: entity.name,
+            },
+        });
+        return StudentMap.fromDB(saved);
+    }
+
+    public async findOne(
+        args: IFindOptionsStudentDto,
+    ): Promise<StudentEntity | null> {
+        const student = await this.prismaService.tx.student.findFirst({
+            where: StudentBuilder.build(args)?.where,
+        });
+        if (!student) return null;
+        return StudentMap.fromDB(student);
+    }
+}
