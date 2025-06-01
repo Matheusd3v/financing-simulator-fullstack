280b8adbbfd29a69618a3e35e7977513168e96f3
Matheus <>
2025-06-01 16:25:13 -0300

    feat: add container base and pages in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { mainTheme } from './style/theme';
+import Navbar from './components/navbar';
+import Home from './features/pages/home';
 
 function App() {
   return (
     <ThemeProvider theme={mainTheme}>
-      
+      <Navbar/>
+      <Home/>
     </ThemeProvider>
   )
 }
