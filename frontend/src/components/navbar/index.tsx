614471e200f0dcd0090455bebf1dbeb78b35276e
Matheus <>
2025-06-02 03:34:42 -0300

    feat: add dropdown in navbar

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,19 +1,47 @@
 import { Link } from "react-router-dom";
 import { CardStyle } from "./style";
-import { HomeIcon } from "lucide-react";
+import { HomeIcon, UserIcon } from "lucide-react";
 import { ROUTES } from "../../routes/constants";
+import { useRef, useState } from "react";
+import { hook } from "../../contexts";
+import Button from "../button";
+import Dropdown from "../dropdown";
 
 const Navbar = () => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const { handleLogout, isAuthenticated } = hook.useAuth();
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
     return (
         <CardStyle>
             <div className="logo-container">
-                <Link to="/">
+                <Link to={isAuthenticated ? ROUTES.dashboard : "/"}>
                     <HomeIcon />
                     <p>EduFinance</p>
                 </Link>
             </div>
 
-            <Link className="sign-container" to={ROUTES.auth.login}><p>Entrar</p></Link>
+            {isAuthenticated ? (
+                <Button
+                    ref={buttonRef}
+                    onClick={() => setShowDropdown(!showDropdown)}
+                    variant="primary"
+                >
+                    <UserIcon size={16} />
+                </Button>
+            ) : (
+                <Link className="sign-container" to={ROUTES.auth.login}>
+                    <p>Entrar</p>
+                </Link>
+            )}
+            {showDropdown && (
+                <Dropdown
+                    isOpen={showDropdown}
+                    onClose={() => setShowDropdown(false)}
+                    onLogout={handleLogout}
+                    triggerRef={buttonRef}
+                />
+            )}
         </CardStyle>
     );
 };
